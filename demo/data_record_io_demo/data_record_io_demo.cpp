b16b45dca0feac4d707977bbc27b9653af38da7e
fenglielie <fenglielie@qq.com>
2025-12-07 21:37:27 +0800

    fix bug

--- demo/data_record_io_demo/data_record_io_demo.cpp
+++ demo/data_record_io_demo/data_record_io_demo.cpp
@@ -1,4 +1,4 @@
-#include "allay/data_handler/data_handler.hpp"
+#include "allay/data_record_io/data_record_io.hpp"
 
 #include <fstream>
 
@@ -6,22 +6,22 @@ int main() {
     {
         // 读取 Nodes.txt
         auto fin = std::fstream(PREFIX "/Nodes.csv", std::ios::in);
-        auto nodes = DataHandler<int, double, double>::read(fin, ',');
+        auto nodes = DataRecordIO<int, double, double>::read(fin, ',');
 
         // 写入 tmp_Nodes.csv
         auto fout = std::fstream(PREFIX "/tmp_Nodes.csv", std::ios::out);
-        DataHandler<int, double, double>::write(fout, nodes, ' ');
+        DataRecordIO<int, double, double>::write(fout, nodes, ' ');
     }
 
     {
         // 读取 Triangles.txt（有格式错误）
         auto fin = std::fstream(PREFIX "/Triangles.txt", std::ios::in);
         auto triangles =
-            DataHandler<int, int, int, int, double>::read(fin, ' ');
+            DataRecordIO<int, int, int, int, double>::read(fin, ' ');
 
         // 写入 tmp_Triangles.txt（剔除了格式错误）
         auto fout = std::fstream(PREFIX "/tmp_Triangles.txt", std::ios::out);
-        DataHandler<int, int, int, int, double>::write(fout, triangles, ',');
+        DataRecordIO<int, int, int, int, double>::write(fout, triangles, ',');
     }
     return 0;
 }
